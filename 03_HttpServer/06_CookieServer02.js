5829a743708b93e1805e887b92dfdf4c3b166f0f
Ming goo <130649685+llkkmm0823@users.noreply.github.com>
2023-07-19 10:18:49 +0900

    22

--- 03_HttpServer/06_CookieServer02.js
+++ 03_HttpServer/06_CookieServer02.js
@@ -7,20 +7,26 @@ const fs = require('fs').promises;
 
 http.createServer( async (req, res)=>{
     let name='홍길동';
+    let login = false;
 
-    if( req.url === '/login'){
+    if( req.url.startsWith('/login') ){
         // 로그인하려는 유저의 전송된 이름을 쿠키에 저장하고  '/' 로 이동
-        expires.setMinutes(expires.getMinutes() + 1);
+        // console.log(name);
+        const expires = new Date();  // 오늘 날짜 현재 시간 생성 저장
+        expires.setMinutes(expires.getMinutes() + 1);  // 오늘날짜 현재시간 + 1분
         res.writeHead( 302, { 
-            Location: '/',  'Set-Cookie': `name=${name}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
-        }); 
+            Location:'/',  
+            'Set-Cookie' : `name=${encodeURIComponent(name)};Expires=${expires.toGMTString()};HttpOnly;Path=/`,
+        });  // ▲  쿠키에 원하는 정보를 심는 동작
+        login = true;
+        // console.log(login);
         res.end();
-    }else if( req.url === '/'){
+    }else if( login ){  // 첫페이지이거나 로그인후에 다시 돌아거나
         // 쿠키에 로그인정보가 저장되어 있는지 확인한후  '홍길동님 방갑습니다' 를 표시
-        if( cookies.name ){
+        
             res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
             res.end(`${name}님 안녕하세요`); 
-        }else{
+    }else{
             try{
                 const data = await fs.readFile('./06_Login.html');
                 res.end(data);
@@ -28,7 +34,6 @@ http.createServer( async (req, res)=>{
                 res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' } );
                 res.end(e.message);
             }
-        }
     }
 
 }).listen(3000, () => {  console.log('3000 포트에서 서버 대기 중');  });
