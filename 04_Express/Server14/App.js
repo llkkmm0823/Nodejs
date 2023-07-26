a75455eb00515d1d547413690b9d5d8ae1d5059c
Ming goo <130649685+llkkmm0823@users.noreply.github.com>
2023-07-26 14:34:42 +0900

    +++

--- 04_Express/Server14/App.js
+++ 04_Express/Server14/App.js
@@ -1,46 +1,53 @@
 const express = require('express');
-const nunjucks = require('nunjucks');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const path = require('path');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+const passport = require('passport');
+const dateFilter = require('nunjucks-date-filter'); 
+const morgan = require('morgan'); 
 
-// 변수에 저장된 모듈들은 초기설정 등을 실행해서 사용 가능 상태로 설정합니다.
 const app = express();
 app.set('port', process.env.PORT || 3000);
-app.use( express.static(path.join(__dirname, 'public'))); // 스태틱 폴더설정
-// app.use('/up', express.static(path.join(__dirname, 'upload'))); // 스태틱 폴더는 두개이상 만들수 있습니다. 그러려면 접근 경로를 서로 다르게 지정해줍니다 '/up'
-
-const dateFilter = require('nunjucks-date-filter'); 
-// 넌적스에서 사용할 날짜 양식 필터 사용을 위한 모듈
-app.set('view engine', 'html');  // 넌적스 파일 확장자
-let env = nunjucks.configure('views', {express: app, watch: true, }); // 넌적스 설정
+dotenv.config();
+app.set('view engine', 'html'); 
+let env = nunjucks.configure('views', {express: app, watch: true, });
 env.addFilter('date', dateFilter);
+app.use(morgan('dev'));
+app.use( express.static(path.join(__dirname, 'public')));
+app.use( 'img',express.static(path.join(__dirname,'uploads')));
 
-app.use(cookieParser());
-app.use(session({ resave:false,  saveUninitialized:false,  secret:"rkdgmlwns", }));
-app.use(express.json());  // json 데이터 사용 설정
-app.use(express.urlencoded({ extended: false }));  // req.body 에 관한 사용 설정
-
-// app.get('/', (req, res)=>{ res.send('<h1>Node.js 에서 제작한 게시판</h1>');});
-const indexRouter = require('./routers');
-const membersRouter = require('./routers/members');
-const boardsRouter = require('./routers/boards');
-app.use('/', indexRouter);
-app.use('/members', membersRouter);
-app.use('/boards', boardsRouter);
-
-// 데이터베이스에 대한 설정
-const { sequelize } = require('./models');
-
-sequelize.sync({ force:false })
-.then(()=>{ console.log('데이터베이스 연결성공');})
-.catch((err)=>{ 
-    console.error(err);
-});
+app.use(express.json());  
+app.use(express.urlencoded({ extended: false })); 
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({ 
+    resave:false,  
+    saveUninitialized:false,  
+    secret:process.env.COOKIE_SECRET,
+    cookie : {
+        httpOnly : true,
+        secure : false,
+    },
+ })); 
+ //한명의 사용자에 의해 세션이 한 개 이상 값이 저장되면 그 값을 사용하는 클라이언트를 구분할 수 있는 쿠키가 자동으로 클라이언트에
+ //심어지는 쿠키에 대한 설정
+ //세션값이 삭제되거나 브라우저 닫히면 쿠키값도 사라짐
 
 
-// 서버 운영중 라우팅에러 또는 그 외 에러가 발생하면 이동하여 처리될 에러처리 라우터
-app.use((req, res, next) => {
+ const pageRouter = require('./routers/page');
+ const postRouter = require('./routers/post');
+ const authRouter = require('./routers/auth');
+ const userRouter = require('./routers/user');
+ app.use('/', pageRouter);
+ app.use('/post', postRouter);
+ app.use('/auth', authRouter);
+ app.use('/user', userRouter);
+//  app.get('/',(req,res)=>{
+//     res.send('<h1>Node Gram</h1>');
+//  });
+
+ app.use((req, res, next) => {
     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
     error.status = 404;
     next(error);
@@ -50,7 +57,23 @@ app.use((err, req, res, next) => {
     res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
     res.status(err.status || 500);
     console.error(err);
-    res.render('error');
+    res.send('error');
+    //res.render('error');
 });
-// 서버 포트설정 & 대기시작 명령
-app.listen(app.get('port'),()=>{console.log(app.get('port'), ' 포트에서 대기중'); });
+app.listen(app.get('port'),()=>{
+    console.log(app.get('port'), ' 포트에서 대기중'); 
+});
+
+
+
+
+
+// const { sequelize } = require('./models');
+
+// sequelize.sync({ force:false })
+// .then(()=>{ console.log('데이터베이스 연결성공');})
+// .catch((err)=>{ 
+//     console.error(err);
+// });
+
+
