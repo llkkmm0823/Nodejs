90832979ed99915213c5387e02ae745ca37c1065
Ming goo <130649685+llkkmm0823@users.noreply.github.com>
2023-07-26 15:21:04 +0900

    +++++

--- 04_Express/Server14/routers/page.js
+++ 04_Express/Server14/routers/page.js
@@ -5,4 +5,9 @@ router.get('/', (req, res)=>{
     res.render('main', {title : 'NodeGram'});
     });
 
+    router.get('/join',(req, res, next)=>{
+        res.render('join', { title: '회원가입 - NodeGram' });
+    });
+
+
 module.exports = router;
