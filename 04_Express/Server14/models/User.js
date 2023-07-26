21d8f06a68359aa28490120d22949ddff7de458a
Ming goo <130649685+llkkmm0823@users.noreply.github.com>
2023-07-26 17:55:05 +0900

    +++++++++++++++++++++++++

--- 04_Express/Server14/models/User.js
+++ 04_Express/Server14/models/User.js
@@ -1,56 +1,82 @@
-// email(문자 50, 널허용, 고유값 설정), nick(문자30, 널허용안함), password(문자30, 널허용 안함), provider(문자20, 널허용 안함, 기본값:'local'), snsid(문자30, 널허용)
-// 모델명 : User  테이블명:users   나머지 포스트 테이블과 같은 설정을 적용하세요
+const express = require('express');
+const { Post, User, Hashtag } = require('../models');
+// const { isLoggedIn, isNotLoggedIn } = require('./middleware');
 
-const { Sequelize } = require("sequelize");
+const router = express.Router();
 
-module.exports = class User extends Sequelize.Model{
-    static init(sequelize){
-        return super.init({
-            // id 는 자동생성
-            email:{ //일반 회원가입 사용자용 사용자식별 필드
-                type:Sequelize.STRING(50),
-                allowNull:true,
-                unique:true,  //널값끼리는 고유값적용을 하지 않습니다
+
+router.get('/', async (req,res,next)=>{
+    try{
+        // 포스트 검색
+        const posts = await Post.findAll({
+            include : {
+                model:User,
+                attributes:['id', 'nick'],
             },
-            nick:{
-                type:Sequelize.STRING(30),
-                allowNull:false,
-            },
-            password:{
-                type:Sequelize.STRING(200),
-                allowNull:true,
-            },
-            provider:{
-                type:Sequelize.STRING(20),
-                allowNull:false,
-                defaultValue:'local',
-            },
-            snsid:{
-                type:Sequelize.STRING(30),
-                allowNull:true,
-            },
-        },{
-            sequelize,
-            timestamps:true,
-            underscored:false,
-            modelName:'User',
-            tableName:'users',
-            paranoid:true,
-            charset:'utf8',
-            collate:'utf8_general_ci',
+            order:[[ 'createdAt' , 'DESC' ]],
         });
-    }
-    static associate(db){
-        db.User.hasMany(db.Post);
-        db.User.belongsToMany(db.User, { foreignKey:'followingId', as:'Followers', through:'Follow' });
-        db.User.belongsToMany(db.User, { foreignKey:'followerId', as:'Followings', through:'Follow' });
-    }
-};
+        res.render('main', 
+            { 
+                title:'Nodegram',   // 타이틀
+                user:req.user,      // 로그인유저의 객체
+                 // 로그인유저가 없으면 0, 있으면 인원수(length-레코드 갯수)
+                followerCount : req.user ? req.user.Followers.length : 0,   // 팔로워 인원수
+                followingCount : req.user ? req.user.Followings.length : 0,  // 팔로잉 인원수
+                followerIdList : req.user ? req.user.Followings.map(f => f.id) : [] ,
+                // 팔로워들의 아이디 리스트 : 배열
+                posts,              // 전체 포스팅들이 담긴 객체
+            }
+        );
+    }catch(err){
+        console.error(err);
+        next(err);
+    }  
+});
 
-// 유저1이 유저2를 팔로잉한다
-// 유저1(Followers) , 유저2(Followings) 로 레코드 생성
-// 반대로 유저2가 유저1을 필로잉한다
-// 유저2(Followers) , 유저1(Followings) 로 레코드 생성
-// 유저3(Followers) , 유저1(Followings)
-// 유저4(Followers) , 유저1(Followings)
-// 유저4(Followers) , 유저2(Followings)
+
+router.get('/join',  isNotLoggedIn, (req, res, next)=>{
+    res.render('join', { title: '회원가입 - Nodegram' });
+});
+
+
+router.get('/profile',  isLoggedIn, (req, res) => {
+    res.render('profile', { 
+        title: '내 정보 - Nodegram',
+        user:req.user, 
+        followerCount : req.user ? req.user.Followers.length : 0,   // 팔로워 인원수
+        followingCount : req.user ? req.user.Followings.length : 0,  // 팔로잉 인원수
+        followerIdList : req.user ? req.user.Followings.map(f => f.id) : [] ,
+    });
+});
+
+
+
+
+router.get('/hashtag',  isLoggedIn, async (req, res, next) => {
+    const query = req.query.hashtag;
+    if (!query) {
+        return res.redirect('/');  // 도착한 검색어가 없으면 메인으로 돌아갑니다
+    }
+    try {
+        const hashtag = await Hashtag.findOne({ where: { title: query } });  // 해시태그 단어 검색
+        let posts = [];
+        if ( hashtag ) {
+          posts = await hashtag.getPosts({ include: [{ model: User }] });  
+          // 해당 해시태그로 외래키관계로 연결된 게시물 검색
+        }
+        return res.render('main', {
+            title: `${query} | NodeGram`,
+            posts,
+            user:req.user, 
+            followerCount : req.user ? req.user.Followers.length : 0,   
+            followingCount : req.user ? req.user.Followings.length : 0, 
+            followerIdList : req.user ? req.user.Followings.map(f => f.id) : [] ,
+        });
+    } catch (error) {
+        console.error(error);
+        return next(error);
+    }
+});
+
+
+module.exports = router;
--- 04_Express/Server14/routers/post.js
+++ 04_Express/Server14/routers/post.js
@@ -1,3 +1,73 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const { Post, User, Hashtag } = require('../models');
+const { isLoggedIn, isNotLoggedIn } = require('./middleware');
+
+
 const router = express.Router();
+
+try {
+    fs.readdirSync('uploads');
+} catch (error) {
+    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+    fs.mkdirSync('uploads');
+}
+const upload = multer({
+    storage: multer.diskStorage({
+      destination(req, file, cb) {
+        cb(null, 'uploads/');
+      },
+      filename(req, file, cb) {
+        const ext = path.extname(file.originalname);
+        cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+      },
+    }),
+    limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+
+router.post('/img', isLoggedIn , upload.single('img'), (req,res,next)=>{
+    console.log(`/img/${req.file.filename}`);
+    res.json({ url: `/img/${req.file.filename}` });
+}); // 그림만 업로드하고, 저장된 경로를  json 형식으로 되돌려 줍니다.
+
+const upload2 = multer();
+// 폼안에  <input type="file"> 있기때문에 submit 이 동작하면 파일을 한번 더 업로드하려고 합니다
+// 파일 업로드를 생략하기위해 비어있는 multer객체를 만들고  upload2.none() 로 파일 업로드를 생략합니다
+router.post('/', isLoggedIn , upload2.none(), async(req,res)=>{
+    try{
+        const currentPost = await Post.create({
+            content: req.body.content,
+            img: req.body.url,
+            UserId: req.user.id,
+        });
+
+        // 게시물 포스팅 할때 같이 입력한 해시태그를 골라내서, 단어별로 처음 나온 단어를 해시태그 테이블에  insert 하고,   현재 게시물이 어떤 해시태그를 갖고 있는지의 여부를 posthashtags 테이블에 insert 합니다
+
+        const hashtags = req.body.content.match(/#[^\s#]*/g);  
+        // '#'으로 시작해서 빈칸과 '#'이 아닌 곳까지를 단어로 해서 모두 검색
+
+        if( hashtags ){  // 추출한 해시태그가 하나이상 있다면....
+            const result = await Promise.all(
+                hashtags.map((tag)=>{
+                    return Hashtag.findOrCreate({
+                        where: { title: tag.slice(1).toLowerCase() },  
+                        // title 필드값이  해시태그중 하나(tag)의 내용중 slice(1) #을 제외한 나머지 글자와 같은 조건으로 검색
+                        // 같은 title 값이 있으면 있는걸로 리턴, 없으면 Hashtag 테이블에 현재 해시태그로 레코드 추가하고 그값으로 리턴
+                    });
+                }),
+            );
+            await currentPost.addHashtags(result.map(  (r)=>r[0] ));
+            // 지금 추가한 post 게시물에 대한 해시태그들을 posthashtags 테이블에 추가
+        }
+
+        res.redirect('/');
+    }catch(err){
+        console.error(err);
+        next(err);
+    }
+});
+
 module.exports = router;
