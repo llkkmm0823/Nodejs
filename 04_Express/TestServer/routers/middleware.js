db767f950c2fd32d762de5b06452c78be6d78440
Ming goo <130649685+llkkmm0823@users.noreply.github.com>
2023-07-28 10:11:39 +0900

    Update middleware.js

--- 04_Express/TestServer/routers/middleware.js
+++ 04_Express/TestServer/routers/middleware.js
@@ -1,14 +1,6 @@
-exports.isLoggedIn=(req,res,next)=>{
-    if( req.session.loginUser != undefined ){
-        next();
-    }else{
-        res.status(403).send('<h2>로그인이 필요합니다 &nbsp; <a href="/">로그인창으로 이동</a><h2>');
-    }
-};
-exports.isNotLoggedIn=(req,res,next)=>{
-    if( req.session.loginUser == undefined ){
-        next();
-    }else{
-        res.status(403).send('<h2>로그아웃후에 이용할 수 있습니다 &nbsp; <a href="/boards">main 창으로 이동</a><h2>');
-    }
+exports.isNotLoggedIn = (req, res, next) => {
+  if (!req.session.loginUser) {
+    return next();
+  }
+  res.status(403).send('로그인한 사용자는 접근할 수 없습니다.');
 };
