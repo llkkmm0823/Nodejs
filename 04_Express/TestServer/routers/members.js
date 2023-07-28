b02ef7b8257f9d523e2205bbcc57ba8f9e30f4a3
Ming goo <130649685+llkkmm0823@users.noreply.github.com>
2023-07-28 10:10:18 +0900

    Update members.js

--- 04_Express/TestServer/routers/members.js
+++ 04_Express/TestServer/routers/members.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const TextUser = require('../models/TextUser');
 const router = express.Router();
-const user = new TextUser();
-
 
 const { isNotLoggedIn } = require('./middleware');
 
